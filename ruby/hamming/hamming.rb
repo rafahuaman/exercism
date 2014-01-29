e28dcf48413ab74e191098b6e5236c60d5d4be05
Rafael <leafar.huaman@gmail.com>
2014-01-29 21:50:03 +0000

    Finish Hamming

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,5 +1,6 @@
 class Hamming
   def self.compute(base, test)
-    [base.size,test.size].min.times.count { |i| base[i] != test[i] } 
+    max_length_to_compare = [base.size,test.size].min
+    max_length_to_compare.times.count { |i| base[i] != test[i] } 
   end
 end
