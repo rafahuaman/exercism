629e5a20475ee253d541b0632b15e9e700cbf57d
Rafael <leafar.huaman@gmail.com>
2014-06-13 11:49:16 +0000

    Finish first iteration of Bob.js

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,4 +1,16 @@
 function Bob() {
-}
+};
+
+Bob.prototype.hey = function (message) {
+	if (message  == message.toUpperCase() && message != message.toLowerCase() ) {
+		return "Woah, chill out!";
+	} else if (message.slice(-1) == "?") {
+		return "Sure.";
+	} else if (!message || !message.trim() ){
+		return "Fine. Be that way!";
+	} else {
+		return "Whatever.";
+	};
+};
 
 module.exports = Bob;
