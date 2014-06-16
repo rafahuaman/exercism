e1bea2b0a352dfdff14dc8a8f57463b58b3352b3
Rafael <leafar.huaman@gmail.com>
2014-06-16 11:22:00 +0000

    Bob's second interation

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,15 +1,38 @@
-function Bob() {
+function Bob() {};
+
+Bob.prototype.responses = {
+		chill : "Woah, chill out!",
+		answer : "Sure.",
+		angry : "Fine. Be that way!",
+		whatever : "Whatever."
 };
 
+var Message = function(text) {
+	this.text = text;	
+}
+
+Message.prototype.isYelled = function(){
+	return this.text  == this.text.toUpperCase() && this.text != this.text.toLowerCase() ;
+}
+
+Message.prototype.isQuestion = function() {
+	return this.text.slice(-1) == "?";
+}
+
+Message.prototype.isSilence = function() {
+	return !this.text || !this.text.trim()
+}
+
 Bob.prototype.hey = function (message) {
-	if (message  == message.toUpperCase() && message != message.toLowerCase() ) {
-		return "Woah, chill out!";
-	} else if (message.slice(-1) == "?") {
-		return "Sure.";
-	} else if (!message || !message.trim() ){
-		return "Fine. Be that way!";
+	var bobsMessage = new Message(message)
+	if (bobsMessage.isYelled()) {
+		return this.responses.chill;
+	} else if (bobsMessage.isQuestion()) {
+		return this.responses.answer;
+	} else if (bobsMessage.isSilence()){
+		return this.responses.angry;
 	} else {
-		return "Whatever.";
+		return this.responses.whatever;
 	};
 };
 
